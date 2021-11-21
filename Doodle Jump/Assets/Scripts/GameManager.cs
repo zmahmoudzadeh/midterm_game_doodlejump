5dfbc51bd3094bfc820f01edd5d4b04450d638e1
zmahmoudzadeh <zmahmoudzadeh78@gmail.com>
2021-11-21 19:18:42 +0330

    Final

---
+++ Doodle Jump/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject blockPrefab;
+    public int blockCount = 100;
+    [Range(0f, 5f)] public float duplicatePositionY;
+    [Range(0f, 5f)] public float duplicatePositionX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 duplicatePosition = new Vector3();
+        for (int i = 0; i < blockCount; i++)
+        {
+            duplicatePosition.y += Random.Range(duplicatePositionY, 1f + duplicatePositionY);
+            duplicatePosition.x = Random.Range(-duplicatePositionX, duplicatePositionX);
+            Instantiate(blockPrefab, duplicatePosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Doodle Jump/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(0f, 1f)] public float moveAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(moveAmount, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-moveAmount, 0, 0);
+        }
+    }
+    
+}
